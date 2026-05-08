d18c6ae06eed29a7687430ab1c165ceee6aa5561
martdebrandt1 <martdebrandt@gmail.com>
2026-05-08 12:52:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -204,7 +204,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WK 2026 Voorspellingstool</title>
+    <title>WK 2026 Voorspellingstool AZ St Blasius</title>
     <style>
         * { margin: 0; padding: 0; box-sizing: border-box; }
         body {
